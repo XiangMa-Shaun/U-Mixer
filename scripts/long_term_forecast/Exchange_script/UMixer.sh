75bd8dbedfa889361c16f7e882d9dbf37775cf84
XiangMa-Shaun <104669280+XiangMa-Shaun@users.noreply.github.com>
2024-01-04 21:02:50 +0800

    Add files via upload

---
+++ scripts/long_term_forecast/Exchange_script/UMixer.sh
@@ -0,0 +1,100 @@
+export CUDA_VISIBLE_DEVICES=7
+
+model_name=UMixer
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id Exchange_96_96 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --d_model 64 \
+  --d_ff 64 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.001
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id Exchange_96_192 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 192 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --d_model 64 \
+  --d_ff 64 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.001
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id Exchange_96_336 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 336 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --d_model 64 \
+  --d_ff 64 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.001
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id Exchange_96_720 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --d_model 64 \
+  --d_ff 64 \
+  --top_k 5 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.001
